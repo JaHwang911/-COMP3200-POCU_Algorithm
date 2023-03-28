c25f2159d861b80b74cf542daedca8a3980e5649
Ja_Hwang <dmagk560@gmail.com>
2023-03-29 00:08:44 +0900

    Add node

---
+++ Assignment4/src/academy/pocu/comp3500/assignment4/Node.java
@@ -0,0 +1,26 @@
+package academy.pocu.comp3500.assignment4;
+
+import java.util.ArrayList;
+
+public final class Node {
+    private final String title;
+    private final ArrayList<Node> neighbors;
+
+    public Node(String title) {
+        this.title = title;
+        this.neighbors = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void addNeighbor(final Node neighbor) {
+        assert (!this.neighbors.contains(neighbor));
+        this.neighbors.add(neighbor);
+    }
+
+    public ArrayList<Node> getNeighbor() {
+        return this.neighbors;
+    }
+}
--- Assignment4/src/academy/pocu/comp3500/assignment4/Project.java
+++ Assignment4/src/academy/pocu/comp3500/assignment4/Project.java
@@ -2,9 +2,40 @@ package academy.pocu.comp3500.assignment4;
 
 import academy.pocu.comp3500.assignment4.project.Task;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public final class Project {
     public Project(final Task[] tasks) {
+        HashMap<String, Integer> taskIndex = new HashMap<>(tasks.length);
+        Node[] nodes = new Node[tasks.length];
+        Node[] reverseDirectionNodes = new Node[tasks.length];
+        ArrayList<Node> startNodes = new ArrayList<>(tasks.length);
 
+        for (int i = 0; i < tasks.length; ++i) {
+            taskIndex.put(tasks[i].getTitle(), i);
+            nodes[i] = new Node(tasks[i].getTitle());
+            reverseDirectionNodes[i] = new Node(tasks[i].getTitle());
+        }
+
+        for (int i = 0; i < tasks.length; ++i) {
+            List<Task> tempPredecessors = tasks[i].getPredecessors();
+
+            if (tempPredecessors.size() == 0) {
+                startNodes.add(nodes[i]);
+                continue;
+            }
+
+            int toNodeIndex = taskIndex.get(tasks[i].getTitle());
+
+            for (var pre : tempPredecessors) {
+                int fromNodeIndex = taskIndex.get(pre.getTitle());
+
+                nodes[fromNodeIndex].addNeighbor(nodes[toNodeIndex]);
+                reverseDirectionNodes[toNodeIndex].addNeighbor(reverseDirectionNodes[fromNodeIndex]);
+            }
+        }
     }
 
     public int findTotalManMonths(final String task) {
